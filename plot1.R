5114601e1187a6e2c77258a4e9136f806d0282a7
Divyansh Agrawal <agrawaldivyansh462@gmail.com>
2021-01-20 20:10:17 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-hist(data_set$Global_active_power, xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
+hist(data_set$Global_active_power, xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power", col = "red")
 
 dev.copy(png, file = "plot1.png")
 dev.off()
